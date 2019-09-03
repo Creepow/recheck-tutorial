ac8995b2f005872479eff0090527b11ea7f0b420
Pascal Allion <pascal.allion@retest.de>
2019-09-03 16:43:48 +0200

    Test js ignore

--- .retest/recheck.ignore.js
+++ .retest/recheck.ignore.js
@@ -8,8 +8,29 @@
 // You can find more details and example rules at: 
 // https://retest.github.io/docs/recheck/how-ignore-works-in-recheck/
 
+//var fontFamilies = [ [ "system-ui", "Arial" ], [ "-apple-system", "sans-serif" ] ];
+//var baseUrl = /http[s]?:\/\/[\w.:\d\-]*/;
+//
+//function matches(element, diff) {
+//    if (diff.key == "font-family") {
+//        for (var i = 0; i < fontFamilies.length; i++) {
+//            if (contains(fontFamilies[i], diff.expected)) {
+//                return contains(fontFamilies[i], diff.actual);
+//            }
+//        }
+//    }
+//	if (diff.key == "opacity") {
+//        return (Math.abs(diff.expected - diff.actual) <= 10);
+//    }
+//	if (diff.expected != null && diff.actual != null) {
+//        cleanExpected = diff.expected.replace(baseUrl, '');
+//        cleanActual = diff.actual.replace(baseUrl, '');
+//        return cleanExpected === cleanActual;
+//    }
+//    return false;
+//}
+
 var fontFamilies = [ [ "system-ui", "Arial" ], [ "-apple-system", "sans-serif" ] ];
-var baseUrl = /http[s]?:\/\/[\w.:\d\-]*/;
 
 function matches(element, diff) {
     if (diff.key == "font-family") {
@@ -18,14 +39,6 @@ function matches(element, diff) {
                 return contains(fontFamilies[i], diff.actual);
             }
         }
-    }
-	if (diff.key == "opacity") {
-        return (Math.abs(diff.expected - diff.actual) <= 10);
-    }
-	if (diff.expected != null && diff.actual != null) {
-        cleanExpected = diff.expected.replace(baseUrl, '');
-        cleanActual = diff.actual.replace(baseUrl, '');
-        return cleanExpected === cleanActual;
     }
     return false;
 }
