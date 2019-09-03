8d969b4b61bb122f2dc8af836f918749fd578b79
Pascal Allion <pascal.allion@retest.de>
2019-09-03 15:12:21 +0200

    Add recheck api key

--- src/test/java/TestTutorial.java
+++ src/test/java/TestTutorial.java
@@ -1,3 +1,5 @@
+import java.util.prefs.Preferences;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +10,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import de.retest.recheck.Recheck;
 import de.retest.recheck.RecheckImpl;
 import de.retest.recheck.RecheckOptions;
+import de.retest.recheck.Rehub;
+import de.retest.recheck.persistence.CloudPersistence;
 
 public class TestTutorial {
 
@@ -27,11 +31,19 @@ public class TestTutorial {
 	@Test
 	public void google() throws Exception {
 		re.startTest();
-
+		System.out.println(getToken());
 		driver.get("http://google.com");
 		re.check(driver, "open");
 
 		re.capTest();
+	}
+
+	private static String getToken() {
+		final String tokenFromEnvironment = System.getenv(CloudPersistence.RECHECK_API_KEY);
+		final String tokenFromPreferences = Preferences.userNodeForPackage(Rehub.class)
+				.get(CloudPersistence.RECHECK_API_KEY, null);
+
+		return tokenFromEnvironment != null ? tokenFromEnvironment : tokenFromPreferences;
 	}
 
 	@After
