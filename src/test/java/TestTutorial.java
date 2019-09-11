0049924179684364a37fd6371f692a1b512de0e3
Pascal Allion <pascal.allion@retest.de>
2019-09-11 14:08:34 +0200

    Update version

--- src/test/java/TestTutorial.java
+++ src/test/java/TestTutorial.java
@@ -1,5 +1,3 @@
-import java.util.prefs.Preferences;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,8 +8,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import de.retest.recheck.Recheck;
 import de.retest.recheck.RecheckImpl;
 import de.retest.recheck.RecheckOptions;
-import de.retest.recheck.Rehub;
-import de.retest.recheck.persistence.CloudPersistence;
 
 public class TestTutorial {
 	private WebDriver driver;
@@ -19,23 +15,16 @@ public class TestTutorial {
 
 	@Before
 	public void setUp() {
-		re = new RecheckImpl(RecheckOptions.builder().reportUploadEnabled(true).build()); // parameter for report on
-																							// rehub
-
+		re = new RecheckImpl(RecheckOptions.builder().enableReportUpload().build());// parameter for report on
+																					// rehub
 		ChromeOptions options = new ChromeOptions();
-
-		options.addArguments("--headless"); // run Chrome headless for Travis
-		options.addArguments("--window-size=1280,720"); // common window size
-		options.addArguments("--lang=en"); // language english
-
+		options.addArguments("--headless", "--window-size=1280,720");
 		driver = new ChromeDriver(options);
 	}
 
 	@Test
 	public void google() throws Exception {
 		re.startTest();
-
-		System.out.println(getToken()); // get the RECHECK_API_KEY
 
 		driver.get("http://google.com");
 		re.check(driver, "open");
@@ -46,13 +35,5 @@ public class TestTutorial {
 	public void tearDown() {
 		driver.quit();
 		re.cap();
-	}
-
-	private static String getToken() {
-		final String tokenFromEnvironment = System.getenv(CloudPersistence.RECHECK_API_KEY);
-		final String tokenFromPreferences = Preferences.userNodeForPackage(Rehub.class)
-				.get(CloudPersistence.RECHECK_API_KEY, null);
-
-		return tokenFromEnvironment != null ? tokenFromEnvironment : tokenFromPreferences;
 	}
 }
